c7131ddd864a28f2e850456c8618cdb90ff04584
Gabriel Firmino Pires Pereira <gabriel.firmino@compasso.com.br>
2024-05-26 16:44:53 -0300

    feat: base

---
+++ templates/Home/index.tsx
@@ -0,0 +1,11 @@
+import CardItem from "@/components/cards/CardItem"
+
+const HomeTemplate = () => {
+  return (
+    <div className="bg-gray-300 min-w-full min-h-screen bg-bricks bg-cover bg-center">
+      <CardItem />
+    </div>
+  )
+}
+
+export default HomeTemplate
