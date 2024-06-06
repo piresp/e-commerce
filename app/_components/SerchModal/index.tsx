392f029585e783f8af8a620dd44c8209fd08a28e
Gabriel Firmino Pires Pereira <gabriel.firmino@compasso.com.br>
2024-06-05 22:36:19 -0300

    feat: serch modal

---
+++ app/_components/SerchModal/index.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { controllerModalSerch, selectModalControllers } from "@/redux/sliceModals"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+const SerchModal = () => {
+  const dispatch = useDispatch()
+  const { modalSerch } = useSelector(selectModalControllers)
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape" && modalSerch) dispatch(controllerModalSerch());
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+    
+  }, [modalSerch, dispatch]);
+
+  if (!modalSerch) return
+
+  return (
+    <div className={`flex flex-col backdrop-blur-md z-40 w-full h-screen absolute top-0 ${modalSerch ? 'opacity-100' : 'opacity-0'} transition-all delay-500`}>
+      <label className="input input-bordered flex items-center gap-2 w-96 self-center mt-28">
+        <input type="text" className="grow" placeholder="Search" autoFocus onBlur={() => dispatch(controllerModalSerch())} />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+      </label>
+    </div>
+  )
+}
+
+export default SerchModal
