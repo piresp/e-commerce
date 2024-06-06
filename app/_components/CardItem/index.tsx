d09a6ed58be27fd941c1f380245650392334c61f
Gabriel Firmino Pires Pereira <gabriel.firmino@compasso.com.br>
2024-06-05 22:35:49 -0300

    feat: hover style

--- app/_components/CardItem/index.tsx
+++ app/_components/CardItem/index.tsx
@@ -15,20 +15,19 @@ const CardItem = ({ ...rest }: TailwindDivType) => {
     >
       <img
         className="rounded-2xl w-full h-auto z-0 relative"
-        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        src="https://images.tcdn.com.br/img/img_prod/654361/vestido_tomara_que_caia_verde_3813_1_2ffe32dac5c8dae4d7ca7d3079e66cd6.jpg"
         alt="car!"
       />
 
-      <div className={`w-full h-full absolute top-0 flex flex-end`}>
-        <span className="h-fit w-full self-end p-6 text-xl text-black font-medium">
-          R$ 00,00
+      <div className={`w-full h-full absolute top-0 flex flex-end ${!hover ? "opacity-100" : "opacity-0"} duration-500 ease-in-out`}>
+        <span className="h-fit w-full self-end p-6 text-xl text-white font-medium">
+          <span className="px-3 py-2 bg-zinc-800 bg-opacity-45 rounded-lg">R$ 00,00</span>
         </span>
       </div>
 
       <div
-        className={`w-full h-full ${
-          hover ? "opacity-100" : "opacity-0"
-        } bg-black bg-opacity-40 z-20 absolute top-0 p-6 grid grid-cols-[auto_100px] grid-rows-[auto_24px_50px_40px] gap-2 transition-all duration-500 ease-in-out rounded-2xl`}
+        className={`w-full h-full ${hover ? "opacity-100" : "opacity-0"
+          } bg-black bg-opacity-40 z-20 absolute top-0 p-6 grid grid-cols-[auto_100px] grid-rows-[auto_24px_50px_40px] gap-2 transition-all duration-500 ease-in-out rounded-2xl`}
       >
         <span className="text-xl row-start-2 col-span-2">
           Title: title awjd
@@ -37,7 +36,7 @@ const CardItem = ({ ...rest }: TailwindDivType) => {
           Description: awudhaiuwd awdhu awd hiawd hui
         </span>
 
-        <span className="text-xl row-start-4 self-end font-medium">
+        <span className="ml-3 text-xl row-start-4 self-end font-medium">
           R$ 00,00
         </span>
 
@@ -50,5 +49,4 @@ const CardItem = ({ ...rest }: TailwindDivType) => {
 };
 
 export default CardItem;
-// https://images.tcdn.com.br/img/img_prod/654361/vestido_tomara_que_caia_verde_3813_1_2ffe32dac5c8dae4d7ca7d3079e66cd6.jpg
 // https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg
