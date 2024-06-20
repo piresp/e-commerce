bf922f8a41519c56e98072b56926cf89f8d1bb9a
Gabriel Firmino Pires Pereira <gabriel.firmino@compasso.com.br>
2024-06-20 10:50:26 -0300

    'final adjusts'

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -7,13 +7,19 @@ import Serch from "./components/Serch";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { clearFilter } from "@/redux/sliceClothes";
+import useScrollPosition from "@/app/_hooks/useScrollPosition";
+import { useUser } from "@clerk/nextjs";
 
 const Header = () => {
   const router = useRouter();
   const dispatch = useDispatch();
+  const { user } = useUser()
+
+  const scrollPosition = useScrollPosition();
+  const opacity = scrollPosition > 0 ? 'opacity-95' : 'opacity-100';
 
   return (
-    <nav className="flex w-full h-16 bg-zinc-800 justify-around items-center">
+    <nav className={`flex w-full h-16 bg-zinc-800 justify-around items-center sticky top-0 z-50 transition-opacity duration-500 ${opacity}`}>
       <ScissorsIcon
         width="40px"
         height="40px"
@@ -32,6 +38,7 @@ const Header = () => {
         />
         <Serch />
         <Cart />
+        {!!user ? <img src={user.imageUrl} alt="User profile" className="w-8 h-8 rounded-full ml-16 cursor-pointer" onClick={() => router.push('/sign-out')} /> : null}
       </div>
     </nav>
   );
--- app/_components/Modals/FinishBuyModal/index.tsx
+++ app/_components/Modals/FinishBuyModal/index.tsx
@@ -12,6 +12,7 @@ const FinishBuyModal = () => {
   const dispatch = useDispatch();
 
   useEscapeKeyHandler(
+    'Escape',
     () => dispatch(controllerModalFinishBuy()),
     modalFinishPurchase
   );
@@ -20,11 +21,17 @@ const FinishBuyModal = () => {
 
   return (
     <div
-      className={`flex flex-col backdrop-blur-md z-40 w-full h-screen absolute top-0 ${
-        modalFinishPurchase ? "opacity-100" : "opacity-0"
-      } transition-all delay-500`}
+      className={`flex flex-col backdrop-blur-md z-40 w-full h-screen absolute top-0 ${modalFinishPurchase ? "opacity-100" : "opacity-0"
+        } transition-all delay-500`}
     >
-      <div className="rounded-2xl bg-zinc-100 h-5/6 w-5/6 m-auto p-12 overflow-hidden"></div>
+      <div className="rounded-2xl bg-zinc-100 h-5/6 w-5/6 m-auto p-12 overflow-hidden">
+        <span className="text-zinc-950 text-4xl">Pagamento</span>
+        <div className="flex w-full gap-2 mt-8">
+          <div className="flex-1 bg-zinc-200 hover:bg-zinc-300 transition-all duration-500 h-11 text-center content-center rounded-md cursor-pointer"><span className="text-black">Pix</span></div>
+          <div className="flex-1 bg-zinc-200 hover:bg-zinc-300 transition-all duration-500 h-11 text-center content-center rounded-md cursor-pointer"><span className="text-black">Cartão de Crédito</span></div>
+          <div className="flex-1 bg-zinc-200 hover:bg-zinc-300 transition-all duration-500 h-11 text-center content-center rounded-md cursor-pointer"><span className="text-black">Boleto</span></div>
+        </div>
+      </div>
     </div>
   );
 };
